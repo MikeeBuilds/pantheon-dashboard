4d3427730e66fbf883b87c749a476626c6b2d491
Algo Hussle <citizensofandrometta@gmail.com>
2023-07-13 19:12:49 -0500

    initial commit

---
+++ .history/pages/index_20230713185532.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Welcome to My Token Dashboard</h1>
+      <p>This is a dashboard for managing your tokens.</p>
+      <nav>
+        <ul>
+          <li><Link href="/dashboard">Go to Dashboard</Link></li>
+          <li><Link href="/profile">Go to Profile</Link></li>
+        </ul>
+      </nav>
+    </main>
+  )
+}
---
+++ .history/pages/profile_20230713185652.tsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+    return (
+      <div>
+        <h1>Profile</h1>
+        {/* Add your profile content here */}
+      </div>
+    );
+  }
+  
